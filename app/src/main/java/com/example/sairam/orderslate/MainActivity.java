894fe147a16c53095ca74a32749c0adeed515c3f
sairam9999 <vsairamanroid@gmail.com>
2018-04-08 20:45:45 -0400

    Added Multiple Activities

--- app/src/main/java/com/example/sairam/orderslate/MainActivity.java
+++ app/src/main/java/com/example/sairam/orderslate/MainActivity.java
@@ -1,26 +1,41 @@
 package com.example.sairam.orderslate;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.TransitionDrawable;
 import android.net.Uri;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.hitomi.cmlibrary.CircleMenu;
 import com.hitomi.cmlibrary.OnMenuSelectedListener;
 import com.hitomi.cmlibrary.OnMenuStatusChangeListener;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class MainActivity extends AppCompatActivity {
+    ImageView mImgView;
+    Timer mContinuousTransitionDrawableTimer;
+    TransitionDrawable transition;
+    boolean isForward = true;
+   // Context con = this.getApplicationContext();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         CircleMenu circleMenu =  findViewById(R.id.circle_menu);
+
+        final Context con = this.getApplicationContext();
 
         circleMenu.setMainMenu(Color.parseColor("#CDCDCD"), R.mipmap.icon_menu, R.mipmap.icon_cancel)
                 .addSubMenu(Color.parseColor("#258CFF"), R.mipmap.icon_home)
@@ -31,17 +46,52 @@ public class MainActivity extends AppCompatActivity {
                 .setOnMenuSelectedListener(new OnMenuSelectedListener() {
 
                     @Override
-                    public void onMenuSelected(int index) {}
+                    public void onMenuSelected(int index) {
+                       // Toast.makeText(con, "abc"+index,Toast.LENGTH_LONG).show();
 
-                }).setOnMenuStatusChangeListener(new OnMenuStatusChangeListener() {
+                       Log.d("sairam","####"+index);
 
-            @Override
-            public void onMenuOpened() {}
+                        Intent homeIntent = new Intent(con, HomeActivity.class);
+                        Intent searchIntent = new Intent(con, SearchActivity.class);
+                        Intent navigationIntent = new Intent(con, NavigationActivity.class);
+                        Intent notificationIntent = new Intent(con, NotificationActivity.class);
+                        Intent settingIntent = new Intent(con, SettingsActivity.class);
+
+//                        EditText editText = (EditText) findViewById(R.id.editText);
+//                        String message = editText.getText().toString();
+//                        intent.putExtra(EXTRA_MESSAGE, message);
+                        switch(index){
+                            case 0:
+                                startActivity(homeIntent);
+                                break;
+                            case 1:
+                                startActivity(searchIntent);
+                                break;
+                            case 2:
+                                startActivity(navigationIntent);
+                                break;
+                            case 3:
+                                startActivity(notificationIntent);
+                                break;
+                            case 4:
+                                startActivity(settingIntent);
+                                break;
+                        }
+
+
+                    }
+
+                });//.setOnMenuStatusChangeListener(new OnMenuStatusChangeListener() {
+
+          /*  @Override
+            public void onMenuOpened() {
+
+            }
 
             @Override
             public void onMenuClosed() {}
-
-        });
+*/
+     //   });
         VideoView videoView =(VideoView)findViewById(R.id.videoView1);
 
         //Creating MediaController
@@ -60,6 +110,23 @@ public class MainActivity extends AppCompatActivity {
         videoView.requestFocus();
         videoView.start();
 
+    }
+    public class ContinuousTransitionDrawableTimerTask extends TimerTask {
+        @Override
+        public void run() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (isForward){
+                        isForward = false;
+                        transition.startTransition(5000);
+                    }else{
+                        isForward = true;
+                        transition.reverseTransition(5000);
+                    }
+                }
+            });
+        }
     }
 }
 
