65ebfcd9540ebffaa714cc9ba282462dc2cc7cb5
sairam9999 <vsairamanroid@gmail.com>
2018-04-16 21:28:57 -0400

    Added Logic for Cart

--- app/src/main/java/com/example/sairam/orderslate/NotificationActivity.java
+++ app/src/main/java/com/example/sairam/orderslate/NotificationActivity.java
@@ -1,13 +1,24 @@
 package com.example.sairam.orderslate;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class NotificationActivity extends AppCompatActivity {
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
+
+public class MenuWithFABActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
-    }
-}
